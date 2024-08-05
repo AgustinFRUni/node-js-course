f8da78dff989b910a7e87dfd3384c65e08e5929c
Agustin Dev <agustinfruni@gmail.com>
2024-08-04 23:52:10 -0300

    Added function to verify token

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,28 @@
 import { Router } from 'express'
 import { update, deleteOne, getAll, getById } from '../controllers/bookController'
+import { verify } from 'jsonwebtoken'
+const { JWT_SECRET_WORD } = process.env
 export const router = Router()
 
 router.get('/', getAll)
 
 router.get('/:id', getById)
 
-router.patch('/:id', update)
+router.patch('/:id', validateUser, update)
 
-router.delete('/:id', deleteOne)
+router.delete('/:id', validateUser, deleteOne)
+
+function validateUser (req, res, next) {
+  const token = req.header?.authorization?.split(' ')[1]
+  if (token) {
+    verify(token, JWT_SECRET_WORD, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ succes: false, message: 'Invalid or Expired token' })
+      }
+      req.decoded = decoded
+      next()
+    })
+  } else {
+    res.status(401).json({ succes: false, message: 'No token provided' })
+  }
+}
